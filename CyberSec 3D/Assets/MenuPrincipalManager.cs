4b7961e1495a02881b60e08f75b092f61e496014
Felipe Ferreira <felipe.dev803@gmail.com>
2024-09-19 22:43:51 -0300

    Ajuste do menu inicial com som

---
+++ CyberSec 3D/Assets/MenuPrincipalManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevel;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    public void Inicar()
+    {
+        SceneManager.LoadScene(nomeDoLevel);
+    }
+
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do jogo");
+        Application.Quit();
+    }
+}
